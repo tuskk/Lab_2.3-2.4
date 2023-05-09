9e3f0b15dc464a866d4a6d16174b64a3e171a2d1
Mega <Mega@TUF15>
2023-05-09 23:35:25 +0300

    Добавьте файлы проекта.

---
+++ Lab_2.3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Lab_2._3
+{
+    class Strings
+    {
+        public virtual int Length()
+        {
+            return 0;
+        }
+
+        public virtual void Replace()
+        {
+            Console.WriteLine("Base class Replace method");
+        }
+    }
+
+    class Characters : Strings
+    {
+        private string value;
+
+        public Characters(string value)
+        {
+            this.value = value;
+        }
+
+        public override int Length()
+        {
+            return value.Length;
+        }
+
+        public override void Replace()
+        {
+            value = value.Replace("#", "!!");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class Digits : Strings
+    {
+        private string value;
+
+        public Digits(string value)
+        {
+            this.value = value;
+        }
+
+        public override int Length()
+        {
+            return value.Length;
+        }
+
+        public override void Replace()
+        {
+            value = value.Replace("3", "11");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Repline;
+            do
+            {
+                Console.WriteLine("Enter a string:");
+                string input1 = Console.ReadLine();
+                Characters characters = new Characters(input1);
+
+                Console.WriteLine("Enter another string:");
+                string input2 = Console.ReadLine();
+                Digits digits = new Digits(input2);
+
+                Console.WriteLine("Length of characters: {0}", characters.Length());
+                Console.WriteLine("Length of digits: {0}", digits.Length());
+
+                Console.WriteLine("Replacing characters...");
+                characters.Replace();
+                Console.WriteLine("Characters after replacement:");
+                characters.Print();
+
+                Console.WriteLine("Replacing digits...");
+                digits.Replace();
+                Console.WriteLine("Digits after replacement:");
+                digits.Print();
+                Console.WriteLine("repeat?(1 = yes,0 = no)");
+                Repline = int.Parse(Console.ReadLine());
+            } while (Repline == 1 & Repline != 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_2.4/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Lab_2._4
+{
+    interface IInsertingSymbol
+    {
+        int Length { get; }
+        void Insert(char symbol, int position);
+    }
+
+    class UpperCaseString : IInsertingSymbol
+    {
+        private string value;
+
+        public UpperCaseString(string value)
+        {
+            this.value = value.ToUpper();
+        }
+
+        public int Length
+        {
+            get { return value.Length; }
+        }
+
+        public void Insert(char symbol, int position)
+        {
+            if (position < 0 || position > value.Length)
+            {
+                Console.WriteLine("Invalid position");
+                return;
+            }
+
+            value = value.Insert(position, char.ToUpper(symbol).ToString());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class LowerCaseString : IInsertingSymbol
+    {
+        private string value;
+
+        public LowerCaseString(string value)
+        {
+            this.value = value.ToLower();
+        }
+
+        public int Length
+        {
+            get { return value.Length; }
+        }
+
+        public void Insert(char symbol, int position)
+        {
+            if (position < 0 || position > value.Length)
+            {
+                Console.WriteLine("Invalid position");
+                return;
+            }
+
+            value = value.Insert(position, char.ToLower(symbol).ToString());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Repline;
+            do
+            {
+                Console.Write("Enter an uppercase string: ");
+                string upperInput = Console.ReadLine();
+                UpperCaseString upper = new UpperCaseString(upperInput);
+                Console.WriteLine("Length: {0}", upper.Length);
+
+                Console.Write("Enter a character to insert: ");
+                char upperChar = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Write("Enter the position to insert the character: ");
+                int upperPos = int.Parse(Console.ReadLine());
+                upper.Insert(upperChar, upperPos);
+                upper.Print();
+
+                Console.WriteLine();
+
+                Console.Write("Enter a lowercase string: ");
+                string lowerInput = Console.ReadLine();
+                LowerCaseString lower = new LowerCaseString(lowerInput);
+                Console.WriteLine("Length: {0}", lower.Length);
+
+                Console.Write("Enter a character to insert: ");
+                char lowerChar = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Write("Enter the position to insert the character: ");
+                int lowerPos = int.Parse(Console.ReadLine());
+                lower.Insert(lowerChar, lowerPos);
+                lower.Print();
+                Console.WriteLine("repeat?(1 = yes,0 = no)");
+                Repline = int.Parse(Console.ReadLine());
+            } while (Repline == 1 & Repline != 0);
+            Console.ReadKey();
+        }
+    }
+}
